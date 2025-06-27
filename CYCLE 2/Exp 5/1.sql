83fb88b9cfaf208401341fb9104cc5d1407b63fd
Minna Joby <24mp2277@rit.ac.in>
2025-06-27 15:29:38 +0530

    Create 1.sql

---
+++ CYCLE 2/Exp 5/1.sql
@@ -0,0 +1,3 @@
+SELECT e.first_name, e.last_name, e.department_id, l.city FROM employees e JOIN 
+departments d ON e.department_id = d.department_id JOIN locations l ON d.location_id = 
+l.location_id WHERE l.location_id = 1700;
