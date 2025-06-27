d24c922c03c06130e502430493a823be71313ac8
Minna Joby <24mp2277@rit.ac.in>
2025-06-27 13:55:45 +0530

    Create 3.sql

---
+++ CYCLE 1/Exp 1/3.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Student(roll_no integer,name varchar(20),dob DATE,address text,phone_no 
+varchar(10),blood_grp varchar(20));
+CREATE TABLE Course(Course_id int,Course name varchar (30),course duration int);
