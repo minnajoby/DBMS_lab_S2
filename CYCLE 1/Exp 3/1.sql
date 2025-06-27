0a3d41f2d0f89447818b0f8060b40bb4eac5a1b2
Minna Joby <24mp2277@rit.ac.in>
2025-06-27 14:49:12 +0530

    Create 1.sql

---
+++ CYCLE 1/Exp 3/1.sql
@@ -0,0 +1,2 @@
+INSERT INTO employee(emp_no,emp_name,dob,address,mobile_no,salary,designation,dept_no)values(‘emp1’,’John’,’2000-12-07’,’jawahar street’, ’1234568456’,8000,’computer assistant’,’D04’); 
+INSERT INTO department(dept_no,dept_name)values(‘D01’,’Computer’);
