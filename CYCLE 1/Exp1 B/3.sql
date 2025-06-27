29f2f95ada67bcf54a1d64957460af4e00128a10
Minna Joby <24mp2277@rit.ac.in>
2025-06-27 14:33:59 +0530

    Create 3.sql

---
+++ CYCLE 1/Exp1 B/3.sql
@@ -0,0 +1,2 @@
+create table employee(emp_no varchar(500),emp_name varchar(20),dob date,address text,mobile_no varchar(30),salary int);
+create table department(dept_no varchar(20),dept_name varchar(50), location varchar(100));
