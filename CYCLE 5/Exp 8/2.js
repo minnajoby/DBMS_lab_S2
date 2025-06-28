1b62a03d33f6ffe3580a339b43bb6ae4373a65b4
Minna Joby <24mp2277@rit.ac.in>
2025-06-28 09:23:30 +0530

    Create 2.js

---
+++ CYCLE 5/Exp 8/2.js
@@ -0,0 +1,3 @@
+db.products.insertOne({ "_id" : 1, "name" : "xPhone", "price" : 799, "releaseDate": 
+ISODate("2011-05-14"), "spec" : { "ram" : 4, "screen" : 6.5, "cpu" : 
+2.66},"color":["white","black"],"storage":[64,128,256]});
